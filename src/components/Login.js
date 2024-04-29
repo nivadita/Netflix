d26be98b3d2915a7910f9ad8c03cfc532829f25e
nivadita <nivadita.biet15@gmail.com>
2024-04-29 17:49:22 +0530

    background url corrected

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
 
       <div className="absolute">
         <img
-          src="lhttps://assets.nflxext.com/ffe/siteui/vlv3/6cefb2f5-90be-4f57-adc4-f6c3c579273d/3943990c-f4e0-4147-82ad-f531e2b547f3/IN-en-20240401-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6cefb2f5-90be-4f57-adc4-f6c3c579273d/3943990c-f4e0-4147-82ad-f531e2b547f3/IN-en-20240401-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="logo"
         />
       </div>
